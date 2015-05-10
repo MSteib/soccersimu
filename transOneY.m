0d9aff9bcdab869dcc1d458573f57d659f1b387b
Xiao-Dong Dang <dangxdong@gmail.com>
2015-05-10 16:35:41 +1000

    Create transOneY.m

---
+++ transOneY.m
@@ -0,0 +1,9 @@
+function yyy = transOneY(yy)
+% This function is used to transform y(i,:) like [1,0,0,0,0,0] or [0,0,0,0,0,1] into 
+% a one-column matrix (vector) with y(i) = 1 or 6.
+% The use of this function is to calculate the prediction accuracy.
+% It should help if we can integrate this transformation into cost function in logistic regression,
+% because this will enhance the penalty algorithm.
+% But not in neural network, because the costfunction in neural network has  already implicitly taken this into account.
+yyy = yy(:,1)+2.*yy(:,2)+3.*yy(:,3)+4.*yy(:,4)+5.*yy(:,5)+6.*yy(:,6);
+end
