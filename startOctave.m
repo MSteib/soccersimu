13cfe398eb6c08347021473feca9d61bc47535d8
Xiao-Dong Dang <dangxdong@gmail.com>
2015-05-10 16:42:55 +1000

    Update startOctave.m

--- startOctave.m
+++ startOctave.m
@@ -17,5 +17,15 @@ ytest = transformY(ytest);
 % Now you can build your own machine learning algorithms, train them with X and y,
 % validate and optimise them with Xval and yval, and 
 % test them with Xtest and ytest.
+
+% I also wrote another transformation function, transOneY(), to transform the six column y like [0,0,1,0,0,0]
+% back into a single column y like [3], to facilitate the calculation of prediction accuracy.
+% so if 'predictedy' is the predicted y matrix, you can calculate the accuracy like:
+accuracy = mean(double(transOneY(predictedy) == transOneY(y)))*100;
 % My practice has given a 74% accuracy with logistic regression and 82% with neural network (with a single hidden layer).
 % See how you may improve this. Good luck!
+
+% If instead using transOneY(), you just calculate like:
+accuracy = mean(mean(double(predictedy == y)))*100;
+% your accuracy score will be much higher, but that's only the mean accuracy for each column, 
+% or say, the mean accuracy for each class in a one-vs-all classification problem.
